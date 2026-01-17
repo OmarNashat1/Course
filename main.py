cae6e01f936e00a5f39b6f9a4f774ff69485dd19
OmarNashat1 <omarnashat2004@gmail.com>
2026-01-17 16:37:47 +0200

    createing base and router

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+from routes import base
+
 app = FastAPI()
 
-@app.get("/welcome")
-def welcome():
-    return {"Message":"Hello World"}
+app.include_router(base.base_router)
+
 
